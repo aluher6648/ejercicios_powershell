6094f3b4bc86248c6cb573bff865729dd068bf34
Pablo her <144794621+aluher6648@users.noreply.github.com>
2023-09-21 03:45:15 +0200

    Add files via upload

---
+++ Practica 3/ejercicio2.ps1
@@ -0,0 +1,6 @@
+﻿# pregunta la edad y guarda variable edad
+[int] $edad = Read-host "¿que edad tienes?"
+# Compara si es mayor de 17 años 
+if ($edad -gt 17) { Write-Host "Es mayor de edad"}
+else {Write-Host "No es mayor de edad"}
+      
---
+++ Practica 3/ejercicio4.ps1
@@ -0,0 +1,11 @@
+﻿# Pide que escribas un numero y luego pide otro
+[int]$numero1 = Read-Host " Escribe un numero "
+[int]$numero2 = Read-Host " Escribe otro numero "
+Write-host ¿que deseas hacer? elige una opcion
+Switch ($numero1 , $numero2) {
+        Sumar {+=}
+        Restar {-=}
+        Multiplicar {*}
+        dividir {/}
+        default{}
+}
