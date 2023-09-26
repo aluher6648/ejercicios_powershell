f6412d7242bb323ad76e39a2804ef694a3d946f3
aluher6648 <aluher6648@ieselcaminas.org>
2023-09-26 13:42:39 +0200

    ejercicios de practicas de ASO hasta el numero 10 acabados

---
+++ Practica 3/ejercicio10.ps1
@@ -0,0 +1,11 @@
+﻿#Variables
+[double]$puntuacion= read-host "Introduce Puntuacion trabajador"
+[int]$dinero= $puntuacion*2400
+
+#Bloque Switch - segun la puntuacion escribe el nivel y sueldo
+Switch ($puntuacion) {
+    {($_ -eq 0.0)} {Write-host "Nivel:Inaceptable" "`nDinero a percibir:"$dinero "€"}
+    {($_ -eq 0.4)} {Write-host "Nivel:Aceptable" "`nDinero a percibir:"$dinero "€"}
+    {($_ -ge 0.6)} {Write-host "Nivel:Meritorio" "`nDinero a percibir:"$dinero "€"}
+
+}
