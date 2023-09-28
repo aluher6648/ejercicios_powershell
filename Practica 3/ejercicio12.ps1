1c0d5b44a4884f0b84785b8d78be25230ba7ff09
aluher6648 <aluher6648@ieselcaminas.org>
2023-09-28 10:06:11 +0200

    ejercicios de powershell haciendo el 12 de ejr3

---
+++ Practica 3/ejercicio12.ps1
@@ -0,0 +1,8 @@
+﻿Write-host "Todas nuestras pizzas llevan tomate y mozarella con la base"$tipopizza = Read-host "`n ¿quieres un pizza vegetariana?"
+
+if ($tipopizza ="si" ) 
+  { write-host "`n Elije uno de los ingredientes " 
+               "`n  ++++++++++ INGREDIENTES ++++++++"
+               "`n  Pimiento "
+               "`n  Tofu "
+}
