8330a237ac0fb78868394fc1e781de566ba3b2d5
Pablo her <aluher6648@ieselcaminas.org>
2023-10-02 00:00:43 +0200

    por fin todo OK ejercicios a full subidos 7 y 8

--- Practica 4/ejercicio8.ps1
+++ Practica 4/ejercicio8.ps1
@@ -1,1 +1,7 @@
-﻿
+﻿# pide un numero entero
+[int] $numero = Read-Host "Introduce un número entero"
+#bucle dentro de bucle , solo pide impares formula segundo for
+for ($fila = 1; $fila -le $numero; $fila++) 
+{ for ($columna = (2 * $fila - 1); $columna -ge 1; $columna -= 2) 
+    {Write-Host -NoNewline "$columna "} Write-Host ""}
+
