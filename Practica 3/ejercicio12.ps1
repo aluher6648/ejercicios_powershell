58ce904b7b58f350b8bc7732ded4099b5ae51132
Pablo her <aluher6648@ieselcaminas.org>
2023-10-01 10:45:35 +0200

    ejercicio 3-12 acabado

--- Practica 3/ejercicio12.ps1
+++ Practica 3/ejercicio12.ps1
@@ -1,8 +1,21 @@
-﻿Write-host "Todas nuestras pizzas llevan tomate y mozarella con la base"$tipopizza = Read-host "`n ¿quieres un pizza vegetariana?"
-
-if ($tipopizza ="si" ) 
-  { write-host "`n Elije uno de los ingredientes " 
-               "`n  ++++++++++ INGREDIENTES ++++++++"
+﻿#aviso de contenidoWrite-host "`n  TODAS NUESTRAS PIZZAS LLEVAN MOZZARELA Y TOMATE CON LA BASE"#eleccion opcion pizza
+$opcion = Read-host "`n ¿QUIERES UNA PIZZA VEGETARIANA? SI/NO"
+#depende de la contestacion $opcion  ingredientes vegetarianos y tipo pizza
+if ($opcion -ieq "si" ) 
+  { write-host "`n  ++++++++++ INGREDIENTES ++++++++"
                "`n  Pimiento "
                "`n  Tofu "
-}
+               $ingredienteextra = Read-host "`n Elije uno de los ingredientes "
+               $tipopizza = "vegetariana"
+               }
+#depende de la contestacion $opcion  ingredientes no vegetarianos y tipo pizza
+Else 
+  {Write-Host "`n  ++++++++++ INGREDIENTES ++++++++"
+               "`n  Peperoni "
+               "`n  Jamon "
+               "`n  Salmon "
+               $ingredienteextra = Read-host "`n Elije uno de los ingredientes "
+               $tipopizza = "no vegetariana"
+               }
+#muestra el tipo de pizza y eleccion igredientes
+Write-host "`n HAS SELECCIONADO PIZZA" $tipopizza  "CON MOZZARELLA , TOMATE Y" $ingredienteextra
