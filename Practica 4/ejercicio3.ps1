41377d59bada56ccf0e342bc9c8ebfc92a2201bc
Pablo her <aluher6648@ieselcaminas.org>
2023-10-01 23:28:31 +0200

    por fin todo OK ejercicios a full subidos 7 y 8

--- Practica 4/ejercicio3.ps1
+++ Practica 4/ejercicio3.ps1
@@ -2,7 +2,7 @@
 [int]$numero = Read-host "Escribe un numero entero y positivo"
 [int]$contador = 0
 
-#
+#resta desde numero hasta contador por pantalla con comas y misma linea
 While ($numero -ge $contador) {
    Write-Host -NoNewline  " $numero, "
    $numero--
