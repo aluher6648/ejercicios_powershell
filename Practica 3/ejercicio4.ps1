a8a612b02489605386e2a942b57587a6a889745f
aluher6648 <aluher6648@ieselcaminas.org>
2023-10-02 09:19:11 +0200

    reparando calculadora 3-4

--- Practica 3/ejercicio4.ps1
+++ Practica 3/ejercicio4.ps1
@@ -1,11 +1,18 @@
 ﻿# Pide que escribas un numero y luego pide otro
-[int]$numero1 = Read-Host " Escribe un numero "
-[int]$numero2 = Read-Host " Escribe otro numero "
-Write-host ¿que deseas hacer? elige una opcion
-Switch ($numero1 , $numero2) {
-        Sumar {+=}
-        Restar {-=}
-        Multiplicar {*}
-        dividir {/}
-        default{}
+[double]$numero1 = Read-Host " Escribe un numero "
+[double]$numero2 = Read-Host " Escribe otro numero " 
+write-host "`n++++++++++++CALCULADORA++++++++++++" 
+Write-Host "`nSumar"
+Write-Host "`nRestar"
+Write-Host "`nMultiplicar"
+Write-Host "`nDividir"  
+$resultado = read-host "`n¿que deseas hacer? elige una opcion"
+
+Switch ($resultado) {
+        Sumar {$resultado = $numero1 + $numero2 }
+        Restar {$resultado = $numero1 - $numero2 }
+        Multiplicar {$resultado = $numero1 * $numero2 }
+        dividir {$resultado = $numero1 / $numero2 }
+     
 }
+   Write-Host "$resultado"
