2f5288e13b31294b971b0997cbe845646e6274a4
aluher6648 <aluher6648@ieselcaminas.org>
2023-10-10 12:53:54 +0200

    practica 3 reparada , final funciona

--- UT2/practica3.ps1
+++ UT2/practica3.ps1
@@ -3,7 +3,7 @@ $departamentos =Import-Csv departamentos.csv -Delimiter ";"
 $empleados = import-Csv empleados.csv -Delimiter ";"
 
 #Crear unidad organizativa EMPRESA
-New-ADOrganizationalUnit -Name "EMPRESA" -Path "DC=EMPRESA,DC=LOCAL" -PassThru
+New-ADOrganizationalUnit -Name "EMPRESA" -Path "DC=EMPRESA,DC=LOCAL" 
 
 #Crea Unidades Organizativas Departamentos desde CSV y las organiza en grupos
 foreach($departamento in $departamentos)
@@ -12,8 +12,8 @@ foreach($departamento in $departamentos)
  $desc= $departamento.descripcion
 
       
-    New-ADOrganizationalUnit -Name $depart -Description $desc -Path "OU=EMPRESA,DC=EMPRESA,DC=LOCAL" -PassThru
-    New-ADGroup -Name $depart -GroupCategory Security -GroupScope Global -Path "OU=$depart,OU=EMPRESA,DC=EMPRESA,DC=LOCAL" -PassThru
+    New-ADOrganizationalUnit -Name $depart -Description $desc -Path "OU=EMPRESA,DC=EMPRESA,DC=LOCAL" 
+    New-ADGroup -Name $depart -GroupCategory Security -GroupScope Global -Path "OU=$depart,OU=EMPRESA,DC=EMPRESA,DC=LOCAL" 
 }
 
 #Crea usuarios en Cada OU de departamento y los añade a su grupo correspondiente
@@ -23,6 +23,6 @@ foreach($empleado in $empleados)
   $nombre = $empleado.nombre
   $apellido =$empleado.apellido
    
-    New-ADUser -Name "$nombre" -Path "OU=$departem,OU=EMPRESA,DC=EMPRESA,DC=LOCAL" -SamAccountName "$nombre" -UserPrincipalName "$nombre.$apellido@empresa.local" -AccountPassword (ConvertTo-SecureString "aso2021." -AsPlainText -Force) -GivenName "$nombre" -Surname "$apellido" -ChangePasswordAtLogon $true -Enabled $true -PassThru
+    New-ADUser -Name "$nombre" -Path "OU=$departem,OU=EMPRESA,DC=EMPRESA,DC=LOCAL" -SamAccountName "$nombre" -UserPrincipalName "$nombre.$apellido@empresa.local" -AccountPassword (ConvertTo-SecureString "aso2021." -AsPlainText -Force) -GivenName "$nombre" -Surname "$apellido" -ChangePasswordAtLogon $true -Enabled $true 
 
     }
