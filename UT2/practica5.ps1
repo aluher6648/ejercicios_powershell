627694b1d5de6ed64116877503dfa9ce3bbf3c15
Pablo her <aluher6648@ieselcaminas.org>
2023-10-17 23:33:34 +0200

    permisos ejercicio 5 completada parte 1 y 2 falta 3

---
+++ UT2/practica5.ps1
@@ -0,0 +1,27 @@
+﻿
+# Variables
+$departamentos =Import-Csv departamentos.csv -Delimiter ";"
+$carpetaEMP = "C:\EMPRESA"
+
+
+# Creacion carpeta empresa y  permisos de la misma
+New-Item -Path $carpetaEMP -ItemType Directory
+New-SmbShare -Path $carpetaEMP -Name EMPRESA  -ReadAccess Todos 
+
+# Crear la estructura de carpetas
+foreach($departamento in $departamentos) #lee dentro de departamentos.csv 
+   {
+
+   # Variables lee el departamento y crea hijos de carpetas desde la principal
+   $depart= $departamento.departamento 
+   $CarpetaDEP = Join-Path -Path $carpetaEMP -ChildPath $depart
+
+   # Crea un item nuevo con las variables 
+   New-Item -Path $CarpetaDEP  -ItemType Directory
+
+   
+   # Da permisos y accesos a las carpetas.
+   New-SmbShare -Path $CarpetaDEP -Name $depart -ChangeAccess $depart -FullAccess Administradores -ReadAccess Todos
+
+}
+
