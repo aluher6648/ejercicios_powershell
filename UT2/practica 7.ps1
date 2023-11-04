cf6827f17d7ed0d567a77bacc1a5dda3d503be2c
Pablo her <aluher6648@ieselcaminas.org>
2023-11-04 14:43:56 +0100

    ejercicio 6 FINAL OK funcionando al fin y 7 acabado

--- UT2/practica 7.ps1
+++ UT2/practica 7.ps1
@@ -1,1 +1,16 @@
-﻿
+﻿#importar de csv
+$empleados =Import-Csv empleados.csv -Delimiter ";"
+
+#Bucle carpeta personal todos los emppleados de ese departamento 
+foreach ($empleado in $empleados)
+ 
+{
+    # variables 
+    $nombre = $empleado.nombre
+    $apellido =$empleado.apellido
+    
+    # actualiza la ruta de perfil de cada empleado de ese departamento PERSONAL
+    If ($empleado.departamento -eq "PERSONAL"){
+        Set-ADUser -Identity "$nombre" -ProfilePath "\\empresa-DC1\Empresa_users$\$nombre.$apellido"
+    }
+}
